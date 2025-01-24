8c5bb54bbbf67f40d70ebf5d972951ef79c02d0c
송지영 <jiy0ung_e@sch.ac.kr>
2025-01-24 19:13:40 +0900

    refac: 불필요 어노테이션 제거

--- src/main/java/org/myteam/server/board/repository/BoardRepository.java
+++ src/main/java/org/myteam/server/board/repository/BoardRepository.java
@@ -2,8 +2,6 @@ package org.myteam.server.board.repository;
 
 import org.myteam.server.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
