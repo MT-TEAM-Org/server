ed718666c2accd17cf339a211a2617af4efa77dd
송지영 <jiy0ung_e@sch.ac.kr>
2025-01-24 21:25:46 +0900

    refac: IP 가져오는 uitls 수정

--- src/main/java/org/myteam/server/util/ClientUtils.java
+++ src/main/java/org/myteam/server/util/ClientUtils.java
@@ -10,17 +10,19 @@ public class ClientUtils {
     public static String getRemoteIP(HttpServletRequest request) {
         String ip = request.getHeader("X-FORWARDED-FOR");
 
-        //proxy 환경일 경우
-        if (ip == null || ip.length() == 0) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
-
-        //웹로직 서버일 경우
-        if (ip == null || ip.length() == 0) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-
-        if (ip == null || ip.length() == 0) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
 
