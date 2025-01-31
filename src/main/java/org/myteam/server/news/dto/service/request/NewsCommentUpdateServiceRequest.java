914aba00aabc231c7907236217514b0262a7cb21
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-01-31 15:40:58 +0900

    feat: 뉴스댓글 저장, 수정, 삭제 Service개발

---
+++ src/main/java/org/myteam/server/news/dto/service/request/NewsCommentUpdateServiceRequest.java
@@ -0,0 +1,19 @@
+package org.myteam.server.news.dto.service.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NewsCommentUpdateServiceRequest {
+
+	private Long newsCommentId;
+	private String comment;
+
+	@Builder
+	public NewsCommentUpdateServiceRequest(Long newsCommentId, String comment) {
+		this.newsCommentId = newsCommentId;
+		this.comment = comment;
+	}
+}
---
+++ src/main/java/org/myteam/server/news/dto/service/response/NewsSaveResponse.java
@@ -0,0 +1,34 @@
+package org.myteam.server.news.dto.service.response;
+
+import java.time.LocalDateTime;
+
+import org.myteam.server.news.domain.News;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NewsSaveResponse {
+	private Long id;
+
+	private String title;
+
+	private String thumbImg;
+
+	@Builder
+	public NewsSaveResponse(Long id, String title, String thumbImg, String description, LocalDateTime postDate) {
+		this.id = id;
+		this.title = title;
+		this.thumbImg = thumbImg;
+	}
+
+	public static NewsSaveResponse of(News news) {
+		return NewsSaveResponse.builder()
+			.id(news.getId())
+			.title(news.getTitle())
+			.thumbImg(news.getThumbImg())
+			.build();
+	}
+}
