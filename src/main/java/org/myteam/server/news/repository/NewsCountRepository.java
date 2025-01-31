13790602b46a18166f05ebb307e730ffcdaaf011
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-01-31 11:38:15 +0900

    fix: Repository 어노테이션 추가

--- src/main/java/org/myteam/server/news/repository/NewsCountRepository.java
+++ src/main/java/org/myteam/server/news/repository/NewsCountRepository.java
@@ -2,6 +2,8 @@ package org.myteam.server.news.repository;
 
 import org.myteam.server.news.domain.NewsCount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NewsCountRepository extends JpaRepository<NewsCount, Long> {
 }
