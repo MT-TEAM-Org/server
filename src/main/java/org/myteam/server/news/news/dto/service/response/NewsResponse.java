964c30cd258d3af83aa262533f05f6456bbafd23
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-05 02:33:20 +0900

    feat: 뉴스 상세조회 Service 개발

--- src/main/java/org/myteam/server/news/news/dto/service/response/NewsResponse.java
+++ src/main/java/org/myteam/server/news/news/dto/service/response/NewsResponse.java
@@ -1,7 +1,11 @@
 package org.myteam.server.news.news.dto.service.response;
 
+import java.time.LocalDateTime;
+
+import org.myteam.server.news.news.domain.News;
 import org.myteam.server.news.news.domain.NewsCategory;
 import org.myteam.server.news.news.dto.repository.NewsDto;
+import org.myteam.server.news.newsCount.domain.NewsCount;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -18,20 +22,48 @@ public class NewsResponse {
 
 	private String thumbImg;
 
+	private int recommendCount;
+
+	private int commentCount;
+
+	private int viewCount;
+
+	private LocalDateTime postTime;
+
 	@Builder
-	public NewsResponse(Long id, NewsCategory category, String title, String thumbImg) {
+	public NewsResponse(Long id, NewsCategory category, String title, String thumbImg, int recommendCount,
+		int commentCount,
+		int viewCount, LocalDateTime postTime) {
 		this.id = id;
 		this.category = category;
 		this.title = title;
 		this.thumbImg = thumbImg;
+		this.recommendCount = recommendCount;
+		this.commentCount = commentCount;
+		this.viewCount = viewCount;
+		this.postTime = postTime;
 	}
 
-	public static NewsResponse of(NewsDto newsDto) {
+
+	public static NewsResponse createResponse(NewsDto newsDto) {
 		return NewsResponse.builder()
 			.id(newsDto.getId())
 			.category(newsDto.getCategory())
 			.title(newsDto.getTitle())
 			.thumbImg(newsDto.getThumbImg())
+			.build();
+	}
+
+	public static NewsResponse createResponse(News news, NewsCount newsCount) {
+		return NewsResponse.builder()
+			.id(news.getId())
+			.category(news.getCategory())
+			.title(news.getTitle())
+			.thumbImg(news.getThumbImg())
+			.postTime(news.getPostTime())
+			.recommendCount(newsCount.getRecommendCount())
+			.commentCount(newsCount.getCommentCount())
+			.viewCount(newsCount.getViewCount())
 			.build();
 	}
 }
--- src/main/java/org/myteam/server/news/news/service/NewsReadService.java
+++ src/main/java/org/myteam/server/news/news/service/NewsReadService.java
@@ -7,8 +7,10 @@ import org.myteam.server.news.news.domain.News;
 import org.myteam.server.news.news.dto.repository.NewsDto;
 import org.myteam.server.news.news.dto.service.request.NewsServiceRequest;
 import org.myteam.server.news.news.dto.service.response.NewsListResponse;
+import org.myteam.server.news.news.dto.service.response.NewsResponse;
 import org.myteam.server.news.news.repository.NewsQueryRepository;
 import org.myteam.server.news.news.repository.NewsRepository;
+import org.myteam.server.news.newsCount.service.NewsCountReadService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,22 +18,27 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class NewsReadService {
 
 	private final NewsQueryRepository newsQueryRepository;
 	private final NewsRepository newsRepository;
+	private final NewsCountReadService newsCountReadService;
 
 	public NewsListResponse findAll(NewsServiceRequest newsServiceRequest) {
-		Page<NewsDto> newsPagingList = newsQueryRepository.getNewsList(
-				newsServiceRequest.getCategory(),
-				newsServiceRequest.getOrderType(),
-				newsServiceRequest.toPageable()
-			);
+		Page<NewsDto> newsPagingList = newsQueryRepository.getNewsList(newsServiceRequest);
 
 		return NewsListResponse.createResponse(PageCustomResponse.of(newsPagingList));
 	}
+
+	public NewsResponse findOne(Long newsId) {
+		return NewsResponse.createResponse(
+			findById(newsId),
+			newsCountReadService.findByNewsId(newsId)
+		);
+	}
+
 
 	public News findById(Long newsId) {
 		return newsRepository.findById(newsId)
