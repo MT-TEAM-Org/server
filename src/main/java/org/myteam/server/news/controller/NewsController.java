0f3b5d6afe286d78a1c04a975fa8b74a274e4c9d
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-04 20:06:38 +0900

    feat: 뉴스 추천수 증가 Controller 개발

--- src/main/java/org/myteam/server/news/controller/NewsController.java
+++ src/main/java/org/myteam/server/news/controller/NewsController.java
@@ -4,11 +4,15 @@ import static org.myteam.server.global.web.response.ResponseStatus.*;
 
 import org.myteam.server.global.web.response.ResponseDto;
 import org.myteam.server.news.dto.controller.request.NewsRequest;
+import org.myteam.server.news.dto.service.response.NewsCountMemberResponse;
 import org.myteam.server.news.dto.service.response.NewsListResponse;
+import org.myteam.server.news.dto.service.response.NewsRecommendResponse;
+import org.myteam.server.news.service.NewsCountService;
 import org.myteam.server.news.service.NewsReadService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 public class NewsController {
 
 	private final NewsReadService newsReadService;
+	private final NewsCountService newsCountService;
 
 	@GetMapping
 	public ResponseEntity<ResponseDto<NewsListResponse>> findAll(@RequestBody @Valid NewsRequest request) {
@@ -31,7 +36,13 @@ public class NewsController {
 			newsReadService.findAll(request.toServiceRequest())));
 	}
 
-	// @PatchMapping("/like")
+	@PatchMapping("/recommend/{newsId}")
+	public ResponseEntity<ResponseDto<NewsRecommendResponse>> like(@PathVariable Long newsId) {
+		return ResponseEntity.ok(new ResponseDto<>(
+			SUCCESS.name(),
+			"뉴스 추천 추가 성공",
+			newsCountService.recommendNews(newsId)));
+	}
 
 
 }
