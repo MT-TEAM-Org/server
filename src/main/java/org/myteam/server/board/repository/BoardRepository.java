98121d59e95ac918c7cdc96d5458550f677265c3
송지영 <thdwldud8@gmail.com>
2025-02-06 14:25:55 +0900

    chore: @repository 어노테이션 추가

--- src/main/java/org/myteam/server/board/repository/BoardRepository.java
+++ src/main/java/org/myteam/server/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package org.myteam.server.board.repository;
 
 import org.myteam.server.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
