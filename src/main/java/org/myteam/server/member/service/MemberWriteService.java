1f4fa2f842fab1f38826e0135002599612cabf0f
Kyoungwoong <kyy980708@gmail.com>
2025-02-08 04:09:39 +0900

    feat: 회원 정보 수정 및 삭제

--- src/main/java/org/myteam/server/member/service/MemberWriteService.java
+++ src/main/java/org/myteam/server/member/service/MemberWriteService.java
@@ -38,6 +38,9 @@ public class MemberWriteService {
     private final SecurityReadService securityReadService;
     private final PasswordEncoder passwordEncoder;
     private final MemberActivityRepository memberActivityRepository;
+    private final MemberJpaRepository memberJpaRepository;
+    private final SecurityReadService securityReadService;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 회원 가입
