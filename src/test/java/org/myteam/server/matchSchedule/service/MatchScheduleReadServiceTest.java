aa850ef2dcd2e8079345746ce320959ba10adb3f
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-12 21:03:31 +0900

    test: 뉴스 비관적락으로 인한 트랜잭션 묶어주기

--- src/test/java/org/myteam/server/matchSchedule/service/MatchScheduleReadServiceTest.java
+++ src/test/java/org/myteam/server/matchSchedule/service/MatchScheduleReadServiceTest.java
@@ -4,13 +4,11 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.myteam.server.IntegrationTestSupport;
 import org.myteam.server.match.matchSchedule.domain.MatchCategory;
-import org.myteam.server.match.matchSchedule.dto.service.response.MatchScheduleResponse;
 import org.myteam.server.match.matchSchedule.service.MatchScheduleReadService;
 import org.myteam.server.match.team.domain.Team;
 import org.myteam.server.match.team.domain.TeamCategory;
@@ -21,37 +19,38 @@ class MatchScheduleReadServiceTest extends IntegrationTestSupport {
 	@Autowired
 	private MatchScheduleReadService matchScheduleReadService;
 
-	@DisplayName("경기일정 목록을 조회한다.")
+	@DisplayName("전체 경기일정 목록을 조회한다.")
 	@Test
 	void findSchedulesBetweenDateTest() {
 		Team team1 = createTeam(1, TeamCategory.FOOTBALL);
 		Team team2 = createTeam(2, TeamCategory.FOOTBALL);
-		Team team3 = createTeam(3, TeamCategory.FOOTBALL);
+		Team team3 = createTeam(3, TeamCategory.ESPORTS);
+		Team team4 = createTeam(4, TeamCategory.ESPORTS);
 
 		createMatchSchedule(team1, team2, MatchCategory.FOOTBALL, LocalDate.now().atStartOfDay());
 		createMatchSchedule(team2, team3, MatchCategory.FOOTBALL, LocalDate.now().atStartOfDay());
-		createMatchSchedule(team3, team1, MatchCategory.FOOTBALL, LocalDate.now().atStartOfDay());
+		createMatchSchedule(team3, team4, MatchCategory.ESPORTS, LocalDate.now().atStartOfDay());
 
-		assertThat(matchScheduleReadService.findSchedulesBetweenDate(MatchCategory.FOOTBALL).getList())
+		assertThat(matchScheduleReadService.findSchedulesBetweenDate(null).getList())
 			.extracting(
-				"homeTeam.id", "homeTeam.name", "homeTeam.logo", "homeTeam.category",
-				"awayTeam.id", "awayTeam.name", "awayTeam.logo", "awayTeam.category",
+				"homeTeam.name", "homeTeam.logo", "homeTeam.category",
+				"awayTeam.name", "awayTeam.logo", "awayTeam.category",
 				"category")
 			.containsExactly(
 				tuple(
-					team1.getId(), team1.getName(), team1.getLogo(), team1.getCategory().name(),
-					team2.getId(), team2.getName(), team2.getLogo(), team2.getCategory().name(),
+					team1.getName(), team1.getLogo(), team1.getCategory().name(),
+					team2.getName(), team2.getLogo(), team2.getCategory().name(),
 					MatchCategory.FOOTBALL.name()
 				),
 				tuple(
-					team2.getId(), team2.getName(), team2.getLogo(), team2.getCategory().name(),
-					team3.getId(), team3.getName(), team3.getLogo(), team3.getCategory().name(),
+					team2.getName(), team2.getLogo(), team2.getCategory().name(),
+					team3.getName(), team3.getLogo(), team3.getCategory().name(),
 					MatchCategory.FOOTBALL.name()
 				),
 				tuple(
-					team3.getId(), team3.getName(), team3.getLogo(), team3.getCategory().name(),
-					team1.getId(), team1.getName(), team1.getLogo(), team1.getCategory().name(),
-					MatchCategory.FOOTBALL.name()
+					team3.getName(), team3.getLogo(), team3.getCategory().name(),
+					team4.getName(), team4.getLogo(), team4.getCategory().name(),
+					MatchCategory.ESPORTS.name()
 				)
 			);
 	}
@@ -65,17 +64,49 @@ class MatchScheduleReadServiceTest extends IntegrationTestSupport {
 
 		createMatchSchedule(team1, team2, MatchCategory.FOOTBALL, LocalDate.now().minusDays(1).atStartOfDay());
 		createMatchSchedule(team2, team3, MatchCategory.FOOTBALL, LocalDate.now().atStartOfDay());
-		createMatchSchedule(team3, team1, MatchCategory.FOOTBALL, LocalDate.now().plusDays(1).atTime(LocalTime.of(7, 0)));
+		createMatchSchedule(team3, team1, MatchCategory.FOOTBALL,
+			LocalDate.now().plusDays(1).atTime(LocalTime.of(7, 0)));
 
 		assertThat(matchScheduleReadService.findSchedulesBetweenDate(MatchCategory.FOOTBALL).getList())
 			.extracting(
-				"homeTeam.id", "homeTeam.name", "homeTeam.logo", "homeTeam.category",
-				"awayTeam.id", "awayTeam.name", "awayTeam.logo", "awayTeam.category",
+				"homeTeam.name", "homeTeam.logo", "homeTeam.category",
+				"awayTeam.name", "awayTeam.logo", "awayTeam.category",
 				"category")
 			.containsExactly(
 				tuple(
-					team2.getId(), team2.getName(), team2.getLogo(), team2.getCategory().name(),
-					team3.getId(), team3.getName(), team3.getLogo(), team3.getCategory().name(),
+					team2.getName(), team2.getLogo(), team2.getCategory().name(),
+					team3.getName(), team3.getLogo(), team3.getCategory().name(),
+					MatchCategory.FOOTBALL.name()
+				)
+			);
+	}
+
+	@DisplayName("축구 경기일정만 조회한다.")
+	@Test
+	void findSchedulesBetweenDateFootballTest() {
+		Team team1 = createTeam(1, TeamCategory.FOOTBALL);
+		Team team2 = createTeam(2, TeamCategory.FOOTBALL);
+		Team team3 = createTeam(3, TeamCategory.ESPORTS);
+		Team team4 = createTeam(4, TeamCategory.ESPORTS);
+
+		createMatchSchedule(team1, team2, MatchCategory.FOOTBALL, LocalDate.now().atStartOfDay());
+		createMatchSchedule(team2, team1, MatchCategory.FOOTBALL, LocalDate.now().atStartOfDay());
+		createMatchSchedule(team3, team4, MatchCategory.ESPORTS, LocalDate.now().atStartOfDay());
+
+		assertThat(matchScheduleReadService.findSchedulesBetweenDate(MatchCategory.FOOTBALL).getList())
+			.extracting(
+				"homeTeam.name", "homeTeam.logo", "homeTeam.category",
+				"awayTeam.name", "awayTeam.logo", "awayTeam.category",
+				"category")
+			.containsExactly(
+				tuple(
+					team1.getName(), team1.getLogo(), team1.getCategory().name(),
+					team2.getName(), team2.getLogo(), team2.getCategory().name(),
+					MatchCategory.FOOTBALL.name()
+				),
+				tuple(
+					team2.getName(), team2.getLogo(), team2.getCategory().name(),
+					team1.getName(), team1.getLogo(), team1.getCategory().name(),
 					MatchCategory.FOOTBALL.name()
 				)
 			);
