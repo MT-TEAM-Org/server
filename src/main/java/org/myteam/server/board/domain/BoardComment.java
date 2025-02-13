cfa4de3d99b135c2cdba1edd7e9aad1c90dfef89
Ji Young <80839715+Zy0ung@users.noreply.github.com>
2025-02-14 03:33:58 +0900

    fix: 게시판 댓글 Entity 수정 (#140)
    * feat: 게시글 추천,추천삭제 API 추가 & 게시글 조회 시 로그인 사용자 추천 여부 응답값 추가
    
    * chore: TODO 완료 작업 주석 삭제
    
    * chore: JsonProperty 추가
    
    * chore: BoardType 프론트와 협의한 Enum으로 수정
    
    * feat: 생성일시, 수정일시를 BaseTime을 상속하도록 수정
    
    * feat: 에러 코드 수정
    
    * chore: 생성자 패턴 수정
    
    * chore: ddl-auto none으로 변경
    
    * chore: 파일명 변경, 불필요 import 제거
    
    * chore: 게시글 상세 조회, 목록 조회 시 로그인 안해도 가능하도록 수정
    
    * feat: 게시판 댓글 CRUD 추가 및 파일명 수정
    
    * feat: endPoint수정 및 security 인가 추가
    
    * chore: 파일명 수정
    
    * chore: 게시판 댓글 목록 조회 RequestBody 대신 RequestParam으로 수정
    
    * chore: 게시판 댓글 컨트롤러 RequestMapping 수정
    
    * chore: 게시판 댓글 endpoint 정리
    
    * chore: securityConfig 수정
    
    * chore: if문을 하나로 줄이기, 상수 최상단으로 올리기
    
    * chore: 안쓰는 import 제거
    
    * fix: 게시판 댓글 BaseTime 추가
    
    * fix: 게시판 댓글 Entity 수정

--- src/main/java/org/myteam/server/board/domain/BoardComment.java
+++ src/main/java/org/myteam/server/board/domain/BoardComment.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -26,9 +27,11 @@ public class BoardComment extends BaseTime {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
     private Board board;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "public_id")
     private Member member;
 
     private String imageUrl;
