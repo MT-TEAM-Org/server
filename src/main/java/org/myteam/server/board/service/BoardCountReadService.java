c21fad262a2281c5ba7e6640d76b7ddddb772c58
송지영 <thdwldud8@gmail.com>
2025-02-14 14:21:12 +0900

    fix: readOnly 에러 해결

--- src/main/java/org/myteam/server/board/service/BoardCountReadService.java
+++ src/main/java/org/myteam/server/board/service/BoardCountReadService.java
@@ -6,13 +6,16 @@ import org.myteam.server.board.repository.BoardCountRepository;
 import org.myteam.server.global.exception.ErrorCode;
 import org.myteam.server.global.exception.PlayHiveException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardCountReadService {
 
     private final BoardCountRepository boardCountRepository;
 
+    @Transactional
     public BoardCount findByBoardId(Long boardId) {
         return boardCountRepository.findByBoardId(boardId)
                 .orElseThrow(() -> new PlayHiveException(ErrorCode.BOARD_RECOMMEND_NOT_FOUND));
