b1a4c9e358fe5c8393e68cb4dd0130b284030fa4
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-11 17:31:03 +0900

    feat: 경기일정 전체조회 Security추가

--- src/main/java/org/myteam/server/global/security/config/SecurityConfig.java
+++ src/main/java/org/myteam/server/global/security/config/SecurityConfig.java
@@ -123,6 +123,7 @@ public class SecurityConfig {
             "/api/news/reply/{newReplyId}",
 
             //경기일정
+            "/api/match/schedule",
             "/api/match/schedule/{matchCategory}",
             "/api/match/{matchId}",
             "/api/match/prediction/{matchId}",
