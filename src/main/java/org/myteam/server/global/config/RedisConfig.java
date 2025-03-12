acf240d69d7a89c491e36e5b5e8731032cb065b4
Kyoungwoong <kyy980708@gmail.com>
2025-03-13 02:35:59 +0900

    fix: redis 캐시 설정

--- src/main/java/org/myteam/server/global/config/RedisConfig.java
+++ src/main/java/org/myteam/server/global/config/RedisConfig.java
@@ -6,7 +6,9 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -15,10 +17,14 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
 @EnableCaching
 @RequiredArgsConstructor
 public class RedisConfig {
+
+    private final int EXPIRED_CACHING_TIME = 10;
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -62,6 +68,9 @@ public class RedisConfig {
      */
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        return RedisCacheManager.builder(redisConnectionFactory).build();
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(EXPIRED_CACHING_TIME)))
+                .build();
     }
 }
