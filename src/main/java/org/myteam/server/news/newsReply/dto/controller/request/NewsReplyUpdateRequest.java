d21fd2d1feed5c80fbf1673300903a3a67120e53
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-14 21:46:02 +0900

    feat: 멘션한 사람 publicId 받도록 수정

--- src/main/java/org/myteam/server/news/newsReply/dto/controller/request/NewsReplyUpdateRequest.java
+++ src/main/java/org/myteam/server/news/newsReply/dto/controller/request/NewsReplyUpdateRequest.java
@@ -1,5 +1,7 @@
 package org.myteam.server.news.newsReply.dto.controller.request;
 
+import java.util.UUID;
+
 import org.myteam.server.news.newsReply.dto.service.request.NewsReplyUpdateServiceRequest;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -20,12 +22,15 @@ public class NewsReplyUpdateRequest {
 	private String comment;
 	@Schema(description = "뉴스 대댓글 이미지 (삭제면 null)")
 	private String imgUrl;
+	@Schema(description = "멘션한 사람 ID (삭제면 null)")
+	private UUID mentionedPublicId;
 
 	@Builder
-	public NewsReplyUpdateRequest(Long newsReplyId, String comment, String imgUrl) {
+	public NewsReplyUpdateRequest(Long newsReplyId, String comment, String imgUrl, UUID mentionedPublicId) {
 		this.newsReplyId = newsReplyId;
 		this.comment = comment;
 		this.imgUrl = imgUrl;
+		this.mentionedPublicId = mentionedPublicId;
 	}
 
 	public NewsReplyUpdateServiceRequest toServiceRequest() {
@@ -33,6 +38,7 @@ public class NewsReplyUpdateRequest {
 			.newsReplyId(newsReplyId)
 			.comment(comment)
 			.imgUrl(imgUrl)
+			.mentionedPublicId(mentionedPublicId)
 			.build();
 	}
 }
--- src/main/java/org/myteam/server/news/newsReply/dto/service/request/NewsReplySaveServiceRequest.java
+++ src/main/java/org/myteam/server/news/newsReply/dto/service/request/NewsReplySaveServiceRequest.java
@@ -1,5 +1,7 @@
 package org.myteam.server.news.newsReply.dto.service.request;
 
+import java.util.UUID;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +13,14 @@ public class NewsReplySaveServiceRequest {
 	private String comment;
 	private String ip;
 	private String imgUrl;
+	private UUID mentionedPublicId;
 
 	@Builder
-	public NewsReplySaveServiceRequest(Long newsCommentId, String comment, String ip, String imgUrl) {
+	public NewsReplySaveServiceRequest(Long newsCommentId, String comment, String ip, String imgUrl, UUID mentionedPublicId) {
 		this.newsCommentId = newsCommentId;
 		this.comment = comment;
 		this.ip = ip;
 		this.imgUrl = imgUrl;
+		this.mentionedPublicId = mentionedPublicId;
 	}
 }
