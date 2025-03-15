a1732275c8c14a303427886d36ff5589ee17a8ba
송지영 <thdwldud8@gmail.com>
2025-03-15 12:02:54 +0900

    chore: MediaUtils 도메인으로 수정

--- src/main/java/org/myteam/server/global/util/upload/MediaUtils.java
+++ src/main/java/org/myteam/server/global/util/upload/MediaUtils.java
@@ -1,13 +1,15 @@
 package org.myteam.server.global.util.upload;
 
 public class MediaUtils {
-    private static final String BUCKET_PREFIX = "devbucket/";
+//    private static final String BUCKET_PREFIX = "devbucket/";
+
+    private static final String MEDIA_DOMAIN = "https://media.playhive.co.kr/";
 
     public static String getImagePath(String url) {
-        if (url == null || !url.contains(BUCKET_PREFIX)) {
+        if (url == null || !url.contains(MEDIA_DOMAIN)) {
             return null; // URL이 없거나, 버킷 경로를 포함하지 않으면 null 반환
         }
-        return url.substring(url.indexOf(BUCKET_PREFIX) + BUCKET_PREFIX.length());
+        return url.substring(url.indexOf(MEDIA_DOMAIN) + MEDIA_DOMAIN.length());
     }
 
     public static boolean verifyImageUrlAndRequestImageUrl(String existsUrl, String requestUrl) {
