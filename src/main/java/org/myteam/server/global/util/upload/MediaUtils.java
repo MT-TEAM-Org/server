a04e0e0c4d55a693a46b47920cb5d808947f3abd
송지영 <thdwldud8@gmail.com>
2025-03-17 00:29:32 +0900

    chore: downloadUrl 환경변수 설정

--- src/main/java/org/myteam/server/global/util/upload/MediaUtils.java
+++ src/main/java/org/myteam/server/global/util/upload/MediaUtils.java
@@ -1,18 +1,29 @@
 package org.myteam.server.global.util.upload;
 
+import org.myteam.server.global.exception.ErrorCode;
+import org.myteam.server.global.exception.PlayHiveException;
+
 public class MediaUtils {
 //    private static final String BUCKET_PREFIX = "devbucket/";
 
-    private static final String MEDIA_DOMAIN = System.getenv("AWS_URL");
-
     public static String getImagePath(String url) {
-        if (url == null || !url.contains(MEDIA_DOMAIN)) {
+        String mediaDomain = getMediaDomain();
+
+        if (url == null || !url.contains(mediaDomain)) {
             return null; // URL이 없거나, 버킷 경로를 포함하지 않으면 null 반환
         }
-        return url.substring(url.indexOf(MEDIA_DOMAIN) + MEDIA_DOMAIN.length());
+        return url.substring(url.indexOf(mediaDomain) + mediaDomain.length());
     }
 
     public static boolean verifyImageUrlAndRequestImageUrl(String existsUrl, String requestUrl) {
         return existsUrl != null && !existsUrl.equals(requestUrl);
+    }
+
+    private static String getMediaDomain() {
+        String envValue = System.getenv("AWS_URL");
+        if (envValue == null) {
+            throw new PlayHiveException(ErrorCode.MISSING_ENVIRONMENT_VARIABLE);
+        }
+        return envValue;
     }
 }
