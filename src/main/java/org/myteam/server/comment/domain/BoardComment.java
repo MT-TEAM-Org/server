bcfcf4b4cf395af21016d280dde0b9bd765968f1
송지영 <thdwldud8@gmail.com>
2025-03-20 13:50:59 +0900

    fix: 댓글 테스트

--- src/main/java/org/myteam/server/comment/domain/BoardComment.java
+++ src/main/java/org/myteam/server/comment/domain/BoardComment.java
@@ -1,22 +1,27 @@
 package org.myteam.server.comment.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.myteam.server.board.domain.Board;
 import org.myteam.server.member.entity.Member;
 
 @Entity
-//@DiscriminatorValue("BOARD")
+@DiscriminatorValue("BOARD")
 @NoArgsConstructor
-public class BoardComment extends Comment{
+public class BoardComment extends Comment {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
 
     @Builder
-    public BoardComment(Member member, Member mentionedMember, String comment, String imageUrl, String createdIp, Comment parent, Board board) {
+    public BoardComment(Member member, Member mentionedMember, String comment, String imageUrl, String createdIp,
+                        Comment parent, Board board) {
         super(member, mentionedMember, comment, imageUrl, createdIp, parent, CommentType.BOARD);
         this.board = board;
     }
--- src/main/java/org/myteam/server/comment/domain/Comment.java
+++ src/main/java/org/myteam/server/comment/domain/Comment.java
@@ -1,8 +1,18 @@
 package org.myteam.server.comment.domain;
 
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.myteam.server.global.domain.BaseTime;
@@ -12,7 +22,7 @@ import org.myteam.server.member.entity.Member;
 @NoArgsConstructor
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)  // 단일 테이블 전략 사용
-//@DiscriminatorColumn(name = "d_type") // 타입 구분 컬럼 추가
+@DiscriminatorColumn(name = "d_type") // 타입 구분 컬럼 추가
 public abstract class Comment extends BaseTime {
 
     private static final int MAX_DEPTH = 3; // 최대 대댓글 깊이 제한
