f8867f11e3a028307ec67605e995c3d255716edb
Kyoungwoong <kyy980708@gmail.com>
2025-03-20 22:56:47 +0900

    fix: 환경변수 설정

--- src/main/java/org/myteam/server/global/security/jwt/JwtProperties.java
+++ src/main/java/org/myteam/server/global/security/jwt/JwtProperties.java
@@ -1,18 +1,16 @@
 package org.myteam.server.global.security.jwt;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.stereotype.Component;
 
 @Getter
-@ConfigurationProperties("jwt")
+@Component
 public class JwtProperties {
-    private final String issuer;
-    private final String secretKey;
-
-    @ConstructorBinding
-    public JwtProperties(String issuer, String secretKey) {
-        this.issuer = issuer;
-        this.secretKey = secretKey;
-    }
+    @Value("${jwt.issuer}")
+    private String issuer;
+    @Value("${jwt.secret-key}")
+    private String secretKey;
 }
--- src/main/java/org/myteam/server/oauth2/handler/CustomOauth2SuccessHandler.java
+++ src/main/java/org/myteam/server/oauth2/handler/CustomOauth2SuccessHandler.java
@@ -36,7 +36,7 @@ import static org.myteam.server.member.domain.MemberStatus.*;
 @Slf4j
 @Component
 public class CustomOauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    @Value("${FRONT_URL:http://localhost:3000}")
+    @Value("${app.frontend.url}")
     private String frontUrl;
     private final String frontSignUpPath = "/sign"; // 프론트 회원가입 주소
     private final JwtProvider jwtProvider;
