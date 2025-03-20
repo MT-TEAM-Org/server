b4e95171b77732f8f6245f8b1bec19bed91b5869
송지영 <thdwldud8@gmail.com>
2025-03-20 14:10:38 +0900

    fix: 댓글 에러 테스트 중

--- src/main/java/org/myteam/server/comment/domain/BoardComment.java
+++ src/main/java/org/myteam/server/comment/domain/BoardComment.java
@@ -1,6 +1,5 @@
 package org.myteam.server.comment.domain;
 
-import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
@@ -10,9 +9,7 @@ import lombok.NoArgsConstructor;
 import org.myteam.server.board.domain.Board;
 import org.myteam.server.member.entity.Member;
 
-
 @Entity
-@DiscriminatorValue("BOARD")
 @NoArgsConstructor
 public class BoardComment extends Comment {
 
