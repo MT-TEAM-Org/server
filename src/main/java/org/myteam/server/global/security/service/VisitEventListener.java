982ac7de3205ff08e38e974d2287db7679d6ae06
Kyoungwoong <kyy980708@gmail.com>
2025-03-20 23:24:35 +0900

    fix: 트랜잭션 커밋 이후 서비스 실행

--- src/main/java/org/myteam/server/global/security/service/VisitEventListener.java
+++ src/main/java/org/myteam/server/global/security/service/VisitEventListener.java
@@ -12,6 +12,8 @@ import org.myteam.server.member.repository.MemberRepository;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.util.UUID;
 
@@ -22,8 +24,7 @@ public class VisitEventListener {
 
     private final MemberActivityRepository memberActivityRepository;
 
-    @Transactional
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleUserLoginEvent(UserLoginEvent event) {
         UUID publicId = event.getPublicId();
 
