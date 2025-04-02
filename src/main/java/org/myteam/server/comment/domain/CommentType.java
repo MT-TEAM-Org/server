4fac6676bc1eaefa3a2a5404bbd9d81bcb018bd9
송지영 <thdwldud8@gmail.com>
2025-04-03 00:25:24 +0900

    fix: match 댓글 에러 해결 중

--- src/main/java/org/myteam/server/comment/domain/CommentType.java
+++ src/main/java/org/myteam/server/comment/domain/CommentType.java
@@ -1,5 +1,6 @@
 package org.myteam.server.comment.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -19,4 +20,14 @@ public enum CommentType {
         return entityClass;
     }
 
+    // JSON에서 String을 Enum으로 변환하는 로직 추가
+    @JsonCreator
+    public static CommentType fromString(String value) {
+        for (CommentType type : CommentType.values()) {
+            if (type.name().equalsIgnoreCase(value)) { // 대소문자 무시 변환
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid CommentType: " + value);
+    }
 }
