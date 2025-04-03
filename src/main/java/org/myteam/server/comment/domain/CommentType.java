f4c2e35d76d9751413cdf4724bf0aa8e01c56074
송지영 <thdwldud8@gmail.com>
2025-04-03 15:10:59 +0900

    chore: 필요없는 코드 제거

--- src/main/java/org/myteam/server/comment/domain/CommentType.java
+++ src/main/java/org/myteam/server/comment/domain/CommentType.java
@@ -1,6 +1,5 @@
 package org.myteam.server.comment.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -18,16 +17,5 @@ public enum CommentType {
 
     public Class<? extends Comment> getEntityClass() {
         return entityClass;
-    }
-
-    // JSON에서 String을 Enum으로 변환하는 로직 추가
-    @JsonCreator
-    public static CommentType fromString(String value) {
-        for (CommentType type : CommentType.values()) {
-            if (type.name().equalsIgnoreCase(value)) { // 대소문자 무시 변환
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Invalid CommentType: " + value);
     }
 }
