83ee2b75d2307c8c004b1def436fb68d0da6f930
송지영 <thdwldud8@gmail.com>
2025-04-14 01:21:09 +0900

    test: IntegrationTestSupport 수정

--- src/test/java/org/myteam/server/IntegrationTestSupport.java
+++ src/test/java/org/myteam/server/IntegrationTestSupport.java
@@ -2,81 +2,32 @@ package org.myteam.server;
 
 import static org.mockito.BDDMockito.given;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
-import org.myteam.server.board.domain.Board;
-import org.myteam.server.board.domain.BoardCount;
-import org.myteam.server.board.domain.BoardRecommend;
-import org.myteam.server.board.domain.CategoryType;
-import org.myteam.server.board.repository.BoardCountRepository;
-import org.myteam.server.board.repository.BoardRecommendRepository;
-import org.myteam.server.board.repository.BoardRepository;
 import org.myteam.server.board.service.BoardCountReadService;
 import org.myteam.server.board.service.BoardCountService;
 import org.myteam.server.board.service.BoardReadService;
 import org.myteam.server.board.service.BoardRecommendReadService;
 import org.myteam.server.board.service.BoardService;
-import org.myteam.server.comment.domain.NewsComment;
-import org.myteam.server.comment.repository.CommentRecommendRepository;
-import org.myteam.server.comment.repository.CommentRepository;
 import org.myteam.server.comment.service.CommentReadService;
 import org.myteam.server.comment.service.CommentService;
-import org.myteam.server.global.domain.Category;
-import org.myteam.server.global.util.redis.RedisCountService;
-import org.myteam.server.improvement.domain.Improvement;
-import org.myteam.server.improvement.domain.ImprovementCount;
-import org.myteam.server.improvement.repository.ImprovementCountRepository;
-import org.myteam.server.improvement.repository.ImprovementRepository;
-import org.myteam.server.inquiry.domain.Inquiry;
-import org.myteam.server.inquiry.domain.InquiryCount;
-import org.myteam.server.inquiry.repository.InquiryCountRepository;
-import org.myteam.server.inquiry.repository.InquiryRepository;
 import org.myteam.server.inquiry.service.InquiryReadService;
 import org.myteam.server.inquiry.service.InquiryService;
-import org.myteam.server.match.match.domain.Match;
-import org.myteam.server.match.match.domain.MatchCategory;
-import org.myteam.server.match.match.repository.MatchRepository;
-import org.myteam.server.match.match.service.MatchReadService;
-import org.myteam.server.match.match.service.MatchYoutubeService;
-import org.myteam.server.match.matchPrediction.domain.MatchPrediction;
-import org.myteam.server.match.matchPrediction.repository.MatchPredictionRepository;
-import org.myteam.server.match.team.domain.Team;
-import org.myteam.server.match.team.domain.TeamCategory;
-import org.myteam.server.match.team.repository.TeamRepository;
 import org.myteam.server.member.domain.MemberRole;
 import org.myteam.server.member.domain.MemberStatus;
 import org.myteam.server.member.domain.MemberType;
 import org.myteam.server.member.entity.Member;
-import org.myteam.server.member.repository.MemberActivityRepository;
-import org.myteam.server.member.repository.MemberJpaRepository;
 import org.myteam.server.member.service.MemberReadService;
 import org.myteam.server.member.service.MemberService;
 import org.myteam.server.member.service.SecurityReadService;
 import org.myteam.server.mypage.service.MyPageReadService;
-import org.myteam.server.news.news.domain.News;
-import org.myteam.server.news.news.repository.NewsRepository;
-import org.myteam.server.news.newsCount.domain.NewsCount;
-import org.myteam.server.news.newsCount.repository.NewsCountRepository;
-import org.myteam.server.news.newsCountMember.domain.NewsCountMember;
-import org.myteam.server.news.newsCountMember.repository.NewsCountMemberRepository;
-import org.myteam.server.notice.domain.Notice;
-import org.myteam.server.notice.domain.NoticeCount;
-import org.myteam.server.notice.repository.NoticeCountRepository;
-import org.myteam.server.notice.repository.NoticeRepository;
 import org.myteam.server.recommend.RecommendService;
-import org.myteam.server.upload.config.S3ConfigLocal;
-import org.myteam.server.upload.controller.S3Controller;
-import org.myteam.server.upload.service.StorageService;
-import org.myteam.server.util.slack.service.SlackService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @ActiveProfiles("test")
-public abstract class IntegrationTestSupport extends TestDriverSupport{
+public abstract class IntegrationTestSupport extends TestDriverSupport {
 
     @MockBean
     protected InquiryReadService inquiryReadService;
@@ -106,10 +57,6 @@ public abstract class IntegrationTestSupport extends TestDriverSupport{
     protected CommentReadService commentReadService;
     @Autowired
     protected RecommendService recommendService;
-    @MockBean
-    protected MatchYoutubeService matchYoutubeService;
-    @MockBean
-    protected MatchReadService matchReadService;
 
     @AfterEach
     void tearDown() {
