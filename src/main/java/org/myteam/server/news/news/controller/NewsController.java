6d865a03af7cda38047aa28aa115c7e269e75c10
Ji Young <80839715+Zy0ung@users.noreply.github.com>
2025-04-26 15:38:44 +0900

    fix: 상세 조회 API 호출인 경우 openGraph true로 청하면 카운트 안되도록 수정 (#296)

--- src/main/java/org/myteam/server/news/news/controller/NewsController.java
+++ src/main/java/org/myteam/server/news/news/controller/NewsController.java
@@ -11,7 +11,6 @@ import org.myteam.server.news.news.dto.controller.request.NewsRequest;
 import org.myteam.server.news.news.dto.service.response.NewsListResponse;
 import org.myteam.server.news.news.dto.service.response.NewsResponse;
 import org.myteam.server.news.news.service.NewsReadService;
-import org.myteam.server.news.newsCount.service.NewsCountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
