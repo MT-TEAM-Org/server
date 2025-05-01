4c6d079b34d8f97d3a8d853052ddf8869c5c198f
Kyoungwoong <kyy980708@gmail.com>
2025-05-01 19:47:46 +0900

    fix: exclude url 설정

--- src/main/java/org/myteam/server/global/security/filter/TokenAuthenticationFilter.java
+++ src/main/java/org/myteam/server/global/security/filter/TokenAuthenticationFilter.java
@@ -36,12 +36,14 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final MemberJpaRepository memberJpaRepository;
 
     private static final String[] excludePath = {
-        "/api/token/regenerate",
+        "/api/token/regenerate", "/actuator/prometheus"
     };
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getServletPath();
+        boolean shouldExclude = Arrays.stream(excludePath).anyMatch(path::startsWith);
+        log.info("[shouldNotFilter] path={}, excluded={}", path, shouldExclude); // 로그 추가
         return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
 
