fee015fd959ec47bb8c86bfe6926000be0776a89
Kyoungwoong <kyy980708@gmail.com>
2025-05-01 19:37:22 +0900

    fix: url 변경

--- src/main/java/org/myteam/server/global/security/config/SecurityConfig.java
+++ src/main/java/org/myteam/server/global/security/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
 		/** @brief health check */
 		"/status",
 		/** @brief prometheus config */
-		"/actuator/prometheus",
+		"/actuator/**",
 		// Swagger Documents
 		/** @brief Swagger Docs */
 		"/v3/api-docs/**", "/swagger-ui/**",
