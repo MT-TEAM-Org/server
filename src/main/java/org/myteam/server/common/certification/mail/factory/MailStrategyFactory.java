88af0675524ed26b5a900e06e72cfa62970b0ee8
dong3058 <hwangdonggeun70@gmail.com>
2025-07-05 19:27:03 +0900

    fix: 로그인 인증 관련 보충 및 충돌해결

--- src/main/java/org/myteam/server/common/certification/mail/factory/MailStrategyFactory.java
+++ src/main/java/org/myteam/server/common/certification/mail/factory/MailStrategyFactory.java
@@ -1,20 +1,12 @@
 package org.myteam.server.common.certification.mail.factory;
 
 import lombok.RequiredArgsConstructor;
-<<<<<<< Updated upstream
-=======
-import org.myteam.server.common.certification.mail.strategy.CertifyMailStrategy;
-import org.myteam.server.common.certification.mail.strategy.NotifySuspendStrategy;
-import org.myteam.server.common.certification.mail.strategy.SignUpStrategy;
-import org.myteam.server.common.certification.mail.strategy.TemporaryPasswordMailStrategy;
->>>>>>> Stashed changes
 import org.myteam.server.common.certification.mail.domain.EmailType;
 import org.myteam.server.common.certification.mail.core.MailStrategy;
 import org.myteam.server.global.exception.ErrorCode;
 import org.myteam.server.global.exception.PlayHiveException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
