c2e5adccb540ff7a31e287b855c555162d83cba9
dong3058 <hwangdonggeun70@gmail.com>
2025-07-25 16:28:42 +0900

    fix: import sentence fix

--- src/main/java/org/myteam/server/admin/repository/AdminMemberResearchRepo.java
+++ src/main/java/org/myteam/server/admin/repository/AdminMemberResearchRepo.java
@@ -10,6 +10,10 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.myteam.server.admin.dto.QBoardCountCte;
+import org.myteam.server.admin.dto.QCommentCountCte;
+import org.myteam.server.admin.dto.QMemberReportCte;
+import org.myteam.server.admin.dto.QReportCountCte;
 import org.myteam.server.admin.entity.AdminChangeLog;
 import org.myteam.server.admin.entity.AdminMemo;
 import org.myteam.server.global.util.date.DateFormatUtil;
@@ -18,6 +22,7 @@ import org.myteam.server.member.domain.MemberRole;
 import org.myteam.server.member.domain.MemberStatus;
 import org.myteam.server.member.domain.MemberType;
 import org.myteam.server.member.entity.Member;
+import org.myteam.server.member.entity.QMember;
 import org.myteam.server.report.domain.ReportType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -35,6 +40,14 @@ import java.util.stream.IntStream;
 import static org.myteam.server.admin.dto.MemberSearchRequestDto.RequestMemberDetail;
 import static org.myteam.server.admin.dto.MemberSearchRequestDto.RequestMemberSearch;
 import static org.myteam.server.admin.dto.MemberSearchResponseDto.*;
+import static org.myteam.server.admin.entity.QAdminMemo.adminMemo;
+import static org.myteam.server.board.domain.QBoard.board;
+import static org.myteam.server.board.domain.QBoardCount.boardCount;
+import static org.myteam.server.comment.domain.QComment.comment1;
+import static org.myteam.server.improvement.domain.QImprovement.improvement;
+import static org.myteam.server.member.entity.QMember.member;
+import static org.myteam.server.member.entity.QMemberActivity.memberActivity;
+import static org.myteam.server.report.domain.QReport.report;
 
 @Repository
 @RequiredArgsConstructor
